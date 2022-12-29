a045a44f24b8033fcff0bc6d9c256dc1fc6fbbfd
Shrish Deshpande <shrish.deshpande@icloud.com>
2022-12-29 21:32:23 +0000

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- main.s
+++ main.s
@@ -1,18 +1,54 @@
-global _start
+section .data
+    prompt: db "Enter size of pyramid:", 0xa
+    promptLen: equ  $ - prompt
+    newline: db 0xa
+    char: db "*"
+
+section .bss           ;Uninitialized data
+    size resb 1
 
 section .text
+    global _start
 
 _start:
-    mov rax, 1 ; write
-    mov rdi, 1 ; to stdout
-    mov rsi, msg
-    mov rdx, msglen
+    mov rax, 1            ; write
+    mov rdi, 1            ; to stdout
+    mov rsi, prompt       ; this message
+    mov rdx, promptLen    ; of this length
     syscall
 
-    mov rax, 60 ; exit
-    mov rdi, 0 ; exit code
+    mov rax, 0            ; read
+    mov rdi, 0            ; from stdin
+    mov rsi, size         ; to this location
+    mov rdx, 1            ; read 1 byte
     syscall
 
-section .rodata
-    msg: db "Hello, world!", 10
-    msglen: equ $ - msg
+    mov al, [size]        ; move size to 8 bit register
+    sub al, '0'           ; convert ascii to integer by subtracting '0'
+    mov [size], al        ; set value of size
+
+    mov bl, [size]
+mainloop:
+    cmp bl, 0
+    je exit
+
+    mov rax, 1            ; write
+    mov rdi, 1            ; to stdout
+    mov rsi, char         ; the character
+    mov rdx, 1            ; of 1 byte
+    syscall
+
+    mov rax, 1            ; write
+    mov rdi, 1            ; to stdout
+    mov rsi, newline      ; a newline
+    mov rdx, 1            ; of 1 byte
+    syscall
+
+    dec bl
+    jmp mainloop
+
+exit:
+
+    mov rax, 60           ; exit
+    mov rdi, 0            ; with exit code
+    syscall
